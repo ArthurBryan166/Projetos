749af187e5196783a986402a3e0565084ae162b5
ArthurBryan166 <arthurbryangomes166@gmail.com>
2025-10-29 21:11:05 -0300

    Quase terminei o conversor

---
+++ ConversorDeMoeda/go.mod
@@ -0,0 +1,3 @@
+module modConversor
+
+go 1.25.0
---
+++ ConversorDeMoeda/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	conversor "modConversor/Conversor"
+)
+
+func main() {
+	clear()
+
+	var origem, destino string
+	var valor float64
+	valorDoDestino := conversor.LogDoConversor(valor, origem, destino)
+	fmt.Print("Bem vindo ao Conversor de Moeda\n\nBRL - USD - EUR\n\nDigite o valor na moeda de origem: ")
+	fmt.Scan(&valor)
+	fmt.Print("Digite a moeda de origem: ")
+	fmt.Scan(&origem)
+	fmt.Print("Digite a moeda de destino: ")
+	fmt.Scan(&destino)
+
+	clear()
+
+	fmt.Printf("%v: %v\n%v: %v", origem, valor, destino, valorDoDestino)
+}
+
+// limpar o terminal
+func clear() {
+	fmt.Print("\033[H\033[2J")
+}

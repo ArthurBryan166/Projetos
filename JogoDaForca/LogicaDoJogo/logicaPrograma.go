54b10e3e3ae26f82a78fb25cc24756468c998e19
ArthurBryan166 <arthurbryangomes166@gmail.com>
2025-12-09 13:56:26 -0300

    Quase termínio do Jogo da Forca

--- JogoDaForca/LogicaDoJogo/logicaPrograma.go
+++ JogoDaForca/LogicaDoJogo/logicaPrograma.go
@@ -4,15 +4,44 @@ import "fmt"
 
 func LogDoJogo(palavraSecreta string) {
 	var palavra []string
+	var letra string
 	chances := 10
+	var acertou, acertouLetra bool
 
 	for i := 0; i < len(palavraSecreta); i++ {
 		palavra = append(palavra, "-")
 	}
 
-	fmt.Printf("Chances: %d\n\nPalavra:\n")
-	for _, letras := range palavra{
-		fmt.Print(letras)
+	for chances > 0{
+		acertou = true
+		acertouLetra = false
+		fmt.Printf("Chances: %d\n\nPalavra:\n", chances)
+		for _, letras := range palavra{
+			fmt.Print(letras)
+		}
+		fmt.Print("Digite uma letra: ")
+		fmt.Scan(&letra)
+
+		for i, v := range palavraSecreta{
+			if string(v) == letra{
+				palavra[i] = letra
+				acertouLetra = true
+			}
+		}
+
+		if !acertouLetra{
+			chances--
+		}
+
+		for _, v := range palavra{
+			if v == "-"{
+				acertou = false
+				break
+			}
+		}
+
+		if acertou{
+			chances = 0
+		}
 	}
-	
 }
